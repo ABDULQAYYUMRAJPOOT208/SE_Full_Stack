34fe97f01cc623131eb8f48bf47ef658eff9702f
ABDULQAYYUMRAJPOOT208 <abdulqayyum4144@gmail.com>
2024-11-13 07:06:43 +0500

    Implement cors

--- backend/index.js
+++ backend/index.js
@@ -3,13 +3,18 @@ import dotenv from "dotenv";
 import connection from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 import cors from "cors";
-
 connection();
 dotenv.config();
 const app = express();
 const PORT = 8000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://ecommerceseproject.vercel.app", // specify the frontend's URL
+    methods: ["GET", "POST", "PUT", "DELETE"], // allow necessary methods
+    credentials: true, // if you’re using cookies or other credentials
+  })
+);
 app.use(express.json());
 app.use("/api", userRoutes);
 app.get("/", (req, res) => {
